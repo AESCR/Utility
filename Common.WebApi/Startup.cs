a35c52750726ed17e8522e8e0efc1b43c8ed2587
AESCR <1017530942@qq.com>
2020-11-11 20:19:50 +0800

    Net5

--- Common.WebApi/Startup.cs
+++ Common.WebApi/Startup.cs
@@ -9,8 +9,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
 
 namespace Common.WebApi
 {
@@ -26,13 +24,13 @@ namespace Common.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot(Configuration);
+           // services.AddOcelot(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseOcelot().Wait();
+            //app.UseOcelot().Wait();
         }
     }
 }
