d29f864951e746bb6e6a1e0c7707715d255c3106
AESCR <1017530942@qq.com>
2020-11-29 17:26:51 +0800

    时间戳

--- Common.Utility/Extensions/System/DateTimeExtension.cs
+++ Common.Utility/Extensions/System/DateTimeExtension.cs
@@ -8,83 +8,52 @@ namespace Common.Utility.Extensions.System
         /// 获取时间戳
         /// </summary>
         /// <returns> </returns>
-        public static string GetTimeStamp(this DateTime time)
+        public static long GetTimeStamp(this DateTime time,bool milliseconds=false)
         {
-            TimeSpan ts = time - new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            return Convert.ToInt64(ts.TotalSeconds).ToString();
-        }
-
-        /// <summary>
-        /// 获取格式化时间字符串 HH:mm:ss
-        /// </summary>
-        /// <param name="time"> </param>
-        /// <returns> </returns>
-        public static string HHmmss(this DateTime time)
-        {
-            return time.ToString("HH:mm:ss");
+            var ts = time - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(milliseconds ? ts.TotalSeconds : ts.TotalMilliseconds);
         }
 
         /// <summary>
         /// 获取中文间隔时间差
         /// </summary>
         /// <param name="time"> </param>
+        /// <param name="nowTime"></param>
         /// <returns> </returns>
         public static string TimeSpanChinese(this DateTime time, DateTime? nowTime = null)
         {
-            var now = nowTime.HasValue ? nowTime.Value : DateTime.Now;
+            var now = nowTime ?? DateTime.Now;
             var span = now.Subtract(time);
             var day = 60 * 24;//天
             var hour = 60;
             if (span.Minutes >= day * 4)
             {
-                return string.Format("{0}年{1}月{2}日", time.Year, time.Month, time.Day);
+                return $"{time.Year}年{time.Month}月{time.Day}日";
             }
             else if (span.Minutes >= day * 3 && span.Minutes < day * 4)
             {
-                return string.Format("{0}天前", span.Days);
+                return $"{span.Days}天前";
             }
             else if (span.Minutes >= day * 2 && span.Minutes < day * 3)
             {
-                return string.Format("{0}天前", span.Days);
+                return $"{span.Days}天前";
             }
             else if (span.Minutes > day && span.Minutes < day * 2)
             {
-                return string.Format("{0}天前", span.Days);
+                return $"{span.Days}天前";
             }
             else if (span.Minutes < day && span.Minutes >= hour)
             {
-                return string.Format("{0}小时前", span.Minutes % 60);
+                return $"{span.Minutes % 60}小时前";
             }
             else if (span.Minutes < hour && span.Minutes >= 1)
             {
-                return string.Format("{0}分钟前", span.Minutes);
+                return $"{span.Minutes}分钟前";
             }
             else
             {
                 return "刚刚";
             }
         }
-
-        /*
-                /// <summary>
-                /// 获取格式化时间字符串 yyyy-MM-dd HH:mm:ss
-                /// </summary>
-                /// <param name="time"> </param>
-                /// <returns> </returns>
-                public static string yyyMMddHHmmss(this DateTime time)
-                {
-                    return time.ToString("yyyy-MM-dd HH:mm:ss");
-                }
-
-                /// <summary>
-                /// 获取格式化时间字符串 yyyy/MM/dd HH:mm:ss
-                /// </summary>
-                /// <param name="time"> </param>
-                /// <returns> </returns>
-                public static string yyyMMddHHmmss2(this DateTime time)
-                {
-                    return time.ToString("yyyy/MM/dd HH:mm:ss");
-                }
-        */
     }
 }
